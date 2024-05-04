892d42515955ff1a7371e32f2256e6a7868c696c
putrabudianto <craftgirlsssshopping@gmail.com>
2024-05-04 16:38:58 +0700

    Initial Commit

---
+++ WhatsappMod/WhatsappModApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappModApp.swift
+//  WhatsappMod
+//
+//  Created by Putra MacBook on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
